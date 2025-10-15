babe0273f166daf3f2577b05174ba7c155601e3c
도미닉 <kiljh@me.com>
2025-10-15 19:51:20 +0900

    feat: 입력된 인자를 ,로 스플릿하여 숫자를 더하여 출력하는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.print("결과 : 3");
+        Scanner scanner = new Scanner(System.in);
+        String argument = scanner.nextLine();
+
+        int result = 0;
+        String[] stringArray = argument.split(",");
+        for(int i = 0; i < stringArray.length; i++){
+            result += Integer.parseInt(stringArray[i]);
+        }
+        System.out.printf("결과 : %d", result);
     }
 }
