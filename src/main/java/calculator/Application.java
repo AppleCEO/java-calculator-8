9e0ee37b567550fab3f01e2b1c2df23905b43f4a
도미닉 <kiljh@me.com>
2025-10-19 21:22:14 +0900

    feat: 입력 안내 문자열을 출력하는 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = getInput(args);
         int result = calculateSum(input);
         printResult(result);
